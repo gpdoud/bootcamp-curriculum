957bafea5cf6d82ab694315f06bd4140223061a6
Greg Doud <gpdoud@gmail.com>
2021-09-28 13:54:31 -0400

    Add project files.

---
+++ bootcamp-curriculum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace bootcamp_curriculum
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var gitgithub = new Topic();
+			gitgithub.Name = "Git/GitHub";
+			gitgithub.Category = "SCM";
+			gitgithub.Difficulty = 2;
+			gitgithub.Print();
+
+			var sqlserver = new Topic()
+			{
+				Name = "SQL Server",
+				Category = "Database",
+				Difficulty = 3
+			};
+			sqlserver.Print();
+		}
+	}
+}
---
+++ bootcamp-curriculum/Topic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bootcamp_curriculum
+{
+	class Topic
+	{
+		private static int NextId { get; set; } = 13;
+		public int Id { get; private set; }
+		public string Name { get; set; }
+		public string Category { get; set; }
+		public int Difficulty { get; set; }
+
+		//public void SetId(int id)
+		//{
+		//	this.Id = id;
+		//}
+
+		public void Print()
+		{
+			Console.WriteLine($"{Id} | {Name} | {Category} | {Difficulty}");
+		}
+
+		public Topic()
+		{
+			this.Id = NextId;
+			NextId = NextId + 17;
+		}
+	}
+}
